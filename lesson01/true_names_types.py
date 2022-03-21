07a6adab3ae78135ff2be53f3ed8aa692b95b6e9
gluhovaea <glu.elenica@gmail.com>
2022-03-22 00:09:28 +0300

    lesson01

---
+++ lesson01/true_names_types.py
@@ -0,0 +1,17 @@
+# верно
+person_name = 'Max'
+
+# определяем тип переменной
+print(type(person_name))
+
+age = 30
+print(type(age))
+
+period = 3.2
+print(type(period))
+
+is_good = True
+print(type(is_good))
+
+person = None
+print(type(person))
